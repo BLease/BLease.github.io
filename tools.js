bfc9cd061c9197abc2f9bf58657b24554f089698
BLease <b-lease@kellogg.northwestern.edu>
2017-01-15 23:29:02 -0600

    kenmorehouse

---
+++ tools.js
@@ -0,0 +1,8 @@
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip()
+})
+$('#example').tooltip(options)
+$(function () {
+  $('[data-toggle="popover"]').popover()
+})
+$('#example').popover(options)
